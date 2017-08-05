865e7a80f4744c7e889f4ad090d8ca4fae8b54db
Douglas Mezuraro <douglasmez@gmail.com>
2017-08-05 00:00:28 -0300

    Criado classe "Edge"

---
+++ Prim/src/Entities/Edge.java
@@ -0,0 +1,13 @@
+package Entities;
+
+public class Edge {
+
+    public Vertex a;
+    public Vertex b;
+
+    public Edge(Vertex a, Vertex b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+}
