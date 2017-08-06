4561eafd9e1084185c8c34404d9b42a8a354683a
Douglas Mezuraro <douglasmez@gmail.com>
2017-08-06 17:13:33 -0300

    Corrigido o método "extractMin()"

--- Prim/src/Entities/Graph.java
+++ Prim/src/Entities/Graph.java
@@ -28,17 +28,17 @@ public class Graph {
 
         Edge edge = new Edge(sourceVertex, targetVertex, weight);
 
-        this.edges.add(edge);
-        
         sourceVertex.adjacent.add(targetVertex);
         targetVertex.adjacent.add(sourceVertex);
+
+        this.edges.add(edge);
 
         return edge;
     }
 
     public Vertex getVertex(String name) throws NullPointerException {
         for(Vertex u: this.vertices)
-            if(u.name == name)
+            if(u.name.equals(name))
                 return u;
         
         throw new NullPointerException("Vértice não encontrado!");
@@ -46,35 +46,30 @@ public class Graph {
 
     public Edge getEdge(Vertex source, Vertex target) {
         for(Edge edge: this.edges) {
-            if(edge.source == source) 
-                if(edge.target == target)
+            if(edge.source.name.equals(source.name)) 
+                if(edge.target.name.equals(target.name))
                     return edge;
-            else if(edge.source == target) 
-                if(edge.target == source)
+            
+            if(edge.source.name.equals(target.name)) 
+                if(edge.target.name.equals(source.name))
                     return edge;  
         }
         return null;
     }
 
-    public Vertex extractMin(Queue<Vertex> queue) {
+    public Vertex extractMin(Queue<Vertex> Q) {
         Vertex minVertex = null;
         int minWeight = this.infinite;
 
-        for(Vertex u: queue) {
-            if(minVertex == null) 
+        for(Vertex u: Q) {
+            if((u.color == Color.white) && (u.key < minWeight)) {
                 minVertex = u;
-               
-            for(Vertex v: u.adjacent) {
-                Edge edge = getEdge(u, v);
-
-                if(edge != null) 
-                    if(edge.weight < minWeight) {
-                        minVertex = v; 
-                        minWeight = edge.weight;
-                    }
-            }                      
+                minWeight = u.key;
+            }
         }
-
+        
+        Q.remove(minVertex);
+        minVertex.color = Color.gray;
         return minVertex;
     }
 
@@ -91,20 +86,24 @@ public class Graph {
 
         r.key = 0;
 
-        Queue<Vertex> queue = new LinkedList<>();
+        Queue<Vertex> Q = new LinkedList<>();
+        
+        Q.addAll(this.vertices);
 
-        queue.addAll(this.vertices);
-
-        while(!queue.isEmpty()) {
-            Vertex u = extractMin(queue);
+        while(!Q.isEmpty()) {
+            Vertex u = extractMin(Q);
 
             for(Vertex v: u.adjacent) {
-                if((queue.contains(v)) && (weigth(u, v) < v.key)) {
+                if((Q.contains(v)) && (weigth(u, v) < v.key)) {
                     v.predecessor = u;
-                    v.key = weigth(u, v);
+                    v.key = weigth(u, v);              
                 }
-            }
+            }        
+            
+            if(u.predecessor != null)
+                System.out.println(getEdge(u.predecessor, u));
         }
     }
+    
 
 }
